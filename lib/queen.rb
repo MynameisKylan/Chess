3bb7906e037eb6253fcf76595d5ec90621e8eb72
Kylan Wong <mynameiskylan@gmail.com>
2020-09-21 13:33:53 -0700

    adjust wording, change equality operator to .zero?

--- lib/queen.rb
+++ lib/queen.rb
@@ -2,9 +2,10 @@ require './lib/move_permutation'
 require './lib/MovesGraph'
 
 class Queen
-  # filter for diagonal or straight-line moves
-  @@transformations = MOVES.filter { |p| p[0].abs == p[1].abs || p[0] == 0 || p[1] == 0 }
+  # filter for diagonal or straight-line transformations
+  @@transformations = MOVES.filter { |p| p[0].abs == p[1].abs || p[0].zero? || p[1].zero? }
   @@moves = nil
+
   def initialize()
     if @@moves.nil?
       p 'loading queen moves'
